a80c258e5e6b68c8a8b25c3fdd273837eb99b5f2
Victor Valentim <victorvalentim0803@gmail.com>
2026-07-11 01:45:33 -0300

    fix: permitindo conexoes externas no docker com 0.0.0.0

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const app = require('./src/App'); 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000; // Alinhado com a porta padrão do Render
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`[🚀 Server] Rodando em modo seguro na porta ${PORT}`);
 });
